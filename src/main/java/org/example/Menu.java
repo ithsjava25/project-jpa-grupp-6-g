ce0b8a1aabd20bb017bfdeee6e5e396c23cdf586
Andreas Kaiberger <andreas.kaiberger@gmail.com>
2026-01-09 10:17:58 +0100

    Move CLI-menu logic to separate class Menu.java and add CLI menu structure for booking system
    Co-authored-by: Rickard Ankar <rickard.ankar@iths.se>

---
+++ src/main/java/org/example/Menu.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    private final GuestRepository guestRepo;
+    private final BookingRepository bookingRepo;
+
+    public Menu(GuestRepository guestRepo, BookingRepository bookingRepo) {
+        this.guestRepo = guestRepo;
+        this.bookingRepo = bookingRepo;
+    }
+
+    public void start() {
+        while (true) {
+            System.out.println("\n=== Booking System ===");
+            System.out.println("1) Create booking");
+            System.out.println("2) View all bookings");
+            System.out.println("3) View all bookings by guest");
+            System.out.println("4) View all guests by booking");
+            System.out.println("5) Cancel booking");
+            System.out.println("0) Exit");
+            System.out.print("Choose: ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1" -> createBooking();
+                case "2" -> printBookings();
+                case "3" -> printBookingsByGuest();
+                case "4" -> printGuestsByBooking();
+                case "5" -> removeBooking();
+                case "0" -> {
+                    System.out.println("Goodbye!");
+                    return;
+                }
+                default -> System.out.println("Invalid choice");
+            }
+        }
+    }
+
+
+    public void printBookings(){
+        //todo: call BookingRepository to get all bookings and print to console
+    }
+
+    public void printBookingsByGuest(){
+        //todo: call BookingRepository to get all bookings connected to guest email and print to console
+    }
+
+    public void printGuestsByBooking(){
+        //todo: call BookingRepository to get all guests connected to booking and print to console
+    }
+
+    public void createBooking(){
+        //todo: enter guest and booking information
+        //todo: check for available rooms and choose room(s)
+        //todo: create booking through BookingRepository
+    }
+
+    public void removeBooking(){
+        //todo: call BookingRepository to remove a booking
+    }
+}
+
